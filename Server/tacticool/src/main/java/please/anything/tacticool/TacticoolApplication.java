5bc4fca01f654aa184df82d41c30c7b82ee2054c
Martin Burgos <miburgos@stud.ntnu.no>
2022-03-11 16:22:01 +0100

    server-hello

---
+++ Server/tacticool/src/main/java/please/anything/tacticool/TacticoolApplication.java
@@ -0,0 +1,14 @@
+package please.anything.tacticool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(TacticoolApplication.class);
+		app.run(args);
+	}
+
+}
---
+++ Server/tacticool/src/test/java/please/anything/tacticool/TacticoolApplicationTests.java
@@ -0,0 +1,13 @@
+package please.anything.tacticool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
