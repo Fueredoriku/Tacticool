e81bd2dde30e6e1427c84b88b5d18a50d4333885
fredrlei <fredrlei@stud.ntnu.no>
2022-03-21 23:52:06 +0100

    Made SceneManager a Singleton, added basic Stack operations for scene handling.

--- client/core/src/com/anything/tacticool/view/scene/SceneManager.java
+++ client/core/src/com/anything/tacticool/view/scene/SceneManager.java
@@ -3,15 +3,45 @@ package com.anything.tacticool.view.scene;
 import java.util.Stack;
 
 public class SceneManager {
-    private Stack<Scene> scenes;
 
-    public SceneManager(){
+    private static Stack<Scene> scenes;
+
+    //Singleton boilerplate begins
+    public static volatile SceneManager Singleton;
+
+    private SceneManager(){
+        if (Singleton != null){
+            throw new RuntimeException("Singleton somehow already created, use SceneManager.getInstance() instead.");
+        }
         scenes = new Stack<>();
     }
 
-    public void push(Scene scene){
-        scenes.pop();
+    public static SceneManager getInstance() {
+        if (Singleton == null) {
+            synchronized (SceneManager.class) {
+                if (Singleton == null) Singleton = new SceneManager();
+            }
+        }
+        return Singleton;
+    }
+    //Singleton boilerplate ends
+
+    //Stack handling functions
+    public void Push(Scene scene){
         scenes.push(scene);
     }
 
+    public Scene Pop(){
+        if (scenes.empty()){
+            throw new IllegalStateException("Can't pop scene from stack when stack is empty.");
+        }
+        return scenes.pop();
+    }
+
+    public Scene Peek(){
+        if (scenes.empty()){
+            throw new IllegalStateException("No Scenes to peek at in Stack.");
+        }
+        return scenes.peek();
+    }
 }
