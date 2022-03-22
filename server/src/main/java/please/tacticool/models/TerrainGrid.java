6a98e5d5f1d3809eab525ddb264caa63fab06251
thoraf <thoraf@stud.ntnu.no>
2022-03-22 18:43:09 +0100

    Added method for moving player that doesn't use Coordinate's add method. Added method for populating starting grid with a collection of terrains

--- server/src/main/java/please/tacticool/models/TerrainGrid.java
+++ server/src/main/java/please/tacticool/models/TerrainGrid.java
@@ -3,6 +3,10 @@ package please.tacticool.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import please.tacticool.enums.Terrain;
+import please.tacticool.models.Actors.Actor;
+import please.tacticool.models.Actors.Character;
+
 public class TerrainGrid {
 
     private List<List<Tile>> grid;
@@ -13,33 +17,45 @@ public class TerrainGrid {
         for (int x = 0; x < width; x++) {
             grid.add(new ArrayList<Tile>());
             for (int y = 0; y < depth; y++) {
-                grid.get(x).add(null); // In the future, this should instead create a custom tile
+                grid.get(x).add(null);
             }
         }
     }
 
-    public void changeTile(int x, int y, Tile newTile) {
-        grid.get(x).set(y, newTile);
+    public void changeTile(Coordinate coordinate, Tile newTile) {
+        grid.get(coordinate.getX()).set(coordinate.getY(), newTile);
     }
 
     // Populates an empty board with tiles based on a set of terrains
-    /**
     public void populateStartingTerrain(Terrain[] terrains) {
         try {
             int i = 0;
             for (int y = 0; y < grid.get(0).size(); y++) {
                 for (int x = 0; x < grid.size(); x++) {
-                    grid.get(x).get(y) = new Tile(null, terrains[i]);
+                    grid.get(x).set(y, new Tile(null, terrains[i]));
+                    i++;
                 }
             }
-        } catch (Exception e) {
+        } catch (Exception e) { }
+    }
+
+    public void movePlayer(int playerID, Coordinate targetLocation) {
+        for (List<Tile> l : grid) {
+            for (Tile tile : l) {
+                Actor actor = tile.getActor();
+
+                if (actor instanceof Character) {
+                    if (((Character) actor).getPlayerID() == playerID) {
+                        tile.setActor(null);
+                        getTile(targetLocation).setActor(actor);
+                    }
+                }
+            }
         }
     }
-     */
 
-    public Tile getTile(int x, int y) {
-        return grid.get(x).get(y);
+    public Tile getTile(Coordinate coordinate) {
+        return grid.get(coordinate.getX()).get(coordinate.getY());
     }
-
 
 }
