2bfb4b7cb133a32e302d3dc00e8bc6e917259fbb
larsmathisen <6087277+larsmathisen@users.noreply.github.com>
2022-03-25 16:28:29 +0100

    Remove redundant "public"

--- server/src/main/java/please/tacticool/models/Actors/Movement.java
+++ server/src/main/java/please/tacticool/models/Actors/Movement.java
@@ -13,7 +13,7 @@ public interface Movement extends Action {
      * Gets the type of action this is
      * @return : type of action
      */
-    public default String getType() {
+    default String getType() {
         return actionType;
     }
 
@@ -21,7 +21,7 @@ public interface Movement extends Action {
      * Gets the radius of the tiles this action affects
      * @return : radius of action target
      */
-    public default int getTargetRadius() {
+    default int getTargetRadius() {
         return targetRadius;
     }
 
@@ -29,20 +29,20 @@ public interface Movement extends Action {
      * Gets the cost of performing this action
      * @return : cost of action
      */
-    public default int getCost() {
+    default int getCost() {
         return defaultCost;
-    }
+    } // Throw in Terrain in some way so we can get cost per tile?
 
     /**
      * Gets the priority this action will take
      * @return : priority of action
      */
-    public default int getPriority() {
+    default int getPriority() {
         return 0;
     }
 
     /**
      * Moves the affected Actor object
      */
-    public void move(Coordinate destination); //TODO: implement actual movement
+    void move(Coordinate destination); //TODO: implement actual movement
 }
