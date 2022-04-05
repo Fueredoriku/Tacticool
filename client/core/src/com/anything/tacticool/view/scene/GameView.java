3070b5606e29133997fdb95be82c4687a2c17873
fredrlei <fredrlei@stud.ntnu.no>
2022-04-05 14:03:50 +0200

    Added basic properties to gameView.

--- client/core/src/com/anything/tacticool/view/scene/GameView.java
+++ client/core/src/com/anything/tacticool/view/scene/GameView.java
@@ -1,5 +1,14 @@
 package com.anything.tacticool.view.scene;
 
+import java.util.Stack;
+
 public class GameView extends Scene {
+    private int actionPoints;
+    private Stack chosenActions;
+    //Maybe stack of inputs?:
+    //private Stack<Input> inputs;
+
+    //private TextureHandler textureHandler;
+    //private Playfield playfield;
 
 }
