6c000997cd3fb1984e558bd3b20710f7381ba8aa
fredrlei <fredrlei@stud.ntnu.no>
2022-04-05 14:03:50 +0200

    Added basic properties to gameView.

--- client/core/src/com/anything/tacticool/view/scene/GameView.java
+++ client/core/src/com/anything/tacticool/view/scene/GameView.java
@@ -1,11 +1,19 @@
 package com.anything.tacticool.view.scene;
 
 import com.anything.tacticool.view.util.GridElementIterator;
+import java.util.Stack;
 
 public class GameView extends Scene {
 
     private GridElementIterator tileIterator;
     private GridElementIterator actorIterator;
     private GridElementIterator inputIterator;
+    private int actionPoints;
+    private Stack chosenActions;
+    //Maybe stack of inputs?:
+    //private Stack<Input> inputs;
+
+    //private TextureHandler textureHandler;
+    //private Playfield playfield;
 
 }
