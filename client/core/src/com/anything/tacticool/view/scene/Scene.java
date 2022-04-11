c3162e4643108c00e3d6fd076f1c973e6647586c
fredrlei <fredrlei@stud.ntnu.no>
2022-04-11 14:54:41 +0200

    Made Scenemanager responsible for calling render/dispose functions on spritebatch of active scene.

--- client/core/src/com/anything/tacticool/view/scene/Scene.java
+++ client/core/src/com/anything/tacticool/view/scene/Scene.java
@@ -1,9 +1,19 @@
 package com.anything.tacticool.view.scene;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class Scene extends ApplicationAdapter {
     protected SceneManager sm;
 
     protected Scene(){this.sm = SceneManager.getInstance();}
+
+    public void render(SpriteBatch batch) {
+        batch.begin();
+        batch.end();
+    }
+
+    public void dispose(SpriteBatch batch){
+        batch.dispose();
+    }
 }
