77057b93fd9c985b6ba95a0fb8a6d6fe1c603fb8
fredrlei <fredrlei@stud.ntnu.no>
2022-04-11 15:29:04 +0200

    Updated and tested that SceneManager works as expected. Made temporary code a bit cleaner.

--- client/core/src/com/anything/tacticool/view/scene/MainView.java
+++ client/core/src/com/anything/tacticool/view/scene/MainView.java
@@ -1,5 +1,12 @@
 package com.anything.tacticool.view.scene;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class MainView extends Scene {
 
+    @Override
+    public void render(SpriteBatch batch){
+        //Temporary code to switch directly to gameView as main menu still is not defined.
+        sm.Push(new GameView());
+    }
 }
