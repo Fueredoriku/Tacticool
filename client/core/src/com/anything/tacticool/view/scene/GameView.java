efa00b359767391c6d79fd5eab93d90085d0ee3d
fredrlei <fredrlei@stud.ntnu.no>
2022-04-11 19:39:49 +0200

    Cleanud up render function in scenes, made GameView instantiate basic objects to be reprsented visually.

--- client/core/src/com/anything/tacticool/view/scene/GameView.java
+++ client/core/src/com/anything/tacticool/view/scene/GameView.java
@@ -1,6 +1,10 @@
 package com.anything.tacticool.view.scene;
 
 import com.anything.tacticool.view.util.GridElementIterator;
+import com.anything.tacticool.view.util.Playfield;
+import com.anything.tacticool.view.util.TextureHandler;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import java.util.Stack;
 
 public class GameView extends Scene {
@@ -11,9 +15,20 @@ public class GameView extends Scene {
     private int actionPoints;
     private Stack chosenActions;
     //Maybe stack of inputs?:
-    //private Stack<Input> inputs;
+    private Stack<Input> inputs;
 
-    //private TextureHandler textureHandler;
-    //private Playfield playfield;
+    private TextureHandler textureHandler;
+    private Playfield playfield;
+
+    public GameView(){
+        super();
+        playfield = new Playfield(new String[2]);
+        textureHandler = new TextureHandler(playfield);
+    }
+
+    @Override
+    public void onRender(SpriteBatch batch){
+        textureHandler.renderBoard(playfield, batch);
+    }
 
 }
