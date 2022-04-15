23a62766a477cb204126cf8ee7fbd75ca7446b75
thoraf <thoraf@stud.ntnu.no>
2022-04-15 10:23:13 +0200

    Made skeleton for main menu functionality

--- client/core/src/com/anything/tacticool/view/scene/MainView.java
+++ client/core/src/com/anything/tacticool/view/scene/MainView.java
@@ -1,5 +1,58 @@
 package com.anything.tacticool.view.scene;
 
+
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
 public class MainView extends Scene {
+
+    protected SceneManager sm;
+
+    /**
+     * textField gameID_input
+     * button joinGame
+     * button settings
+     * button quit
+     */
+
+    private TextField gameID_TextField;
+    private ImageButton joinGame_Button;
+    private ImageButton settings_Button;
+
+
+    //constructor
+
+    private void show() {
+        joinGame_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+                joinGame(gameID_TextField.getText());
+            }
+        });
+
+        settings_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                openSettings();
+            }
+        });
+    }
+
+    //Method for joining game
+
+
+    private void joinGame(String gameID) {
+
+    }
+
+    //Method for opening settings
+    private void openSettings() {
+
+    }
+
+    //Method for closing app?
 
 }
