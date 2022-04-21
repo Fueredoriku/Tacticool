e11e04b1cb1025919fc101b9c7b0a00aa6a22394
thoraf <thoraf@stud.ntnu.no>
2022-04-21 12:28:38 +0200

    Added the UI assets, and switched from using the incorrect create() method during setup to using a constructor. Minor refactoring

--- client/core/src/com/anything/tacticool/view/scene/MainView.java
+++ client/core/src/com/anything/tacticool/view/scene/MainView.java
@@ -7,9 +7,9 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 
@@ -21,85 +21,56 @@ enum MenuState {
 
 public class MainView extends Scene {
 
-    protected SceneManager sm;
-
     private Stage stage;
-    private String gameID;
+    private Skin skin;
     private MenuState menuState;
 
+    private String gameID;
 
-    @Override
-    public void create() {
-        this.menuState = MenuState.MAIN;
 
-        this.stage = new Stage(new ScreenViewport());
-
-        // Skin to texture UI elements. Currently uses libgdx's internal skin
-        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-
-        float sw = Gdx.graphics.getWidth();
-        float sh = Gdx.graphics.getHeight();
-
-        // Instantiates UI elements
-        TextField gameID_Input = new TextField("Game ID", skin);
-        TextButton joinGame_Button = new TextButton("Join Game", skin);
-        TextButton settings_Button = new TextButton("Settings", skin);
-
-        // Defines listeners for when the join game and settings buttons are pressed
-        joinGame_Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                joinGame(gameID);
-            }
-        });
-
-        settings_Button.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                openSettings();
-            }
-        });
-
-        // Sets locations of UI elements
-        gameID_Input.setPosition(sw/2f, sh * 4/5, Align.center);
-        joinGame_Button.setPosition(sw/2f, sh * 3/5, Align.center);
-        settings_Button.setPosition(sw/2f, sh * 2/5, Align.center);
-
-        // Adds UI elements to stage
-        stage.addActor(gameID_Input);
-        stage.addActor(joinGame_Button);
-        stage.addActor(settings_Button);
-
-        // Allows UI elements to take inputs
-        Gdx.input.setInputProcessor(stage);
+    public MainView() {
+        prepareVariables();
+        prepareUI();
     }
+
+
 
     @Override
     public void render(SpriteBatch batch) {
-        Gdx.gl.glClearColor(0,0,0,0);
+        ScreenUtils.clear(0,0,0,1);
+
+        batch.begin();
 
         switch (menuState) {
             case MAIN:
-                stage.act();
                 stage.draw();
-
                 checkState();
-
                 this.gameID = ((TextField) stage.getActors().get(0)).getText();
                 break;
+
             case QEUED:
                 //TODO make call to server to check if game can start
+                //TODO change if statement to take the answer from the server as its condition
                 if (true) {
                     this.menuState = menuState.START;
                 }
                 break;
+
             case START:
                 //TODO make call to server to start new game
                 this.menuState = menuState.MAIN;
                 sm.Push(new GameView());
                 break;
         }
+
+        batch.end();
     }
+
+    @Override
+    public void dispose(SpriteBatch batch) {
+        batch.dispose();
+    }
+
 
     // Rudimentary exception handling
     private void checkState() {
@@ -113,13 +84,72 @@ public class MainView extends Scene {
 
 
     //Method for joining game
-    private void joinGame(String gameID) {
+    private void joinGame() {
+        System.out.println(this.gameID);
         this.menuState = menuState.QEUED;
     }
 
     //Method for opening settings
     private void openSettings() {
         sm.Push(new Settings());
+    }
+
+
+    // Two methods used by constructor
+    private void prepareVariables() {
+        this.menuState = MenuState.MAIN;
+        this.stage = new Stage(new ScreenViewport());
+
+        // Skin to texture UI elements. Currently uses libgdx's basic skin
+        this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        // Upscales font
+        skin.getFont("default-font").getData().setScale(3f);
+    }
+    private void prepareUI() {
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float uiWidth = screenWidth/3f;
+        float uiHeight = screenHeight/6f;
+        float ui_xPosition = screenWidth/2 - uiWidth/2;
+
+        // Instantiates UI elements
+        TextField gameID_Input = new TextField("Game ID", skin);
+        TextButton joinGame_Button = new TextButton("Join Game", skin);
+        TextButton settings_Button = new TextButton("Settings", skin);
+
+        // Sets locations of UI elements
+        gameID_Input.setPosition(ui_xPosition, screenHeight * 4/5);
+        joinGame_Button.setPosition(ui_xPosition, screenHeight * 3/5);
+        settings_Button.setPosition(ui_xPosition, screenHeight * 2/5);
+
+        // Sets size of all UI elements
+        gameID_Input.setSize(uiWidth, uiHeight);
+        joinGame_Button.setSize(uiWidth, uiHeight);
+        settings_Button.setSize(uiWidth, uiHeight);
+
+        // Defines listeners for when the join game and settings buttons are pressed
+        joinGame_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                joinGame();
+            }
+        });
+
+        settings_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                openSettings();
+            }
+        });
+
+        // Adds UI elements to stage
+        stage.addActor(gameID_Input);
+        stage.addActor(joinGame_Button);
+        stage.addActor(settings_Button);
+
+        // Allows UI elements to take inputs
+        Gdx.input.setInputProcessor(stage);
     }
 }
 
