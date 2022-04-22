9c2814deaf1be8418a29be83cee9484a676bde9b
thoraf <thoraf@stud.ntnu.no>
2022-04-22 16:11:49 +0200

    Made skeleton for settings view, and generalized Stage handling to abstract class Scene, and SceneManager to solve a bug

--- client/core/src/com/anything/tacticool/view/scene/Scene.java
+++ client/core/src/com/anything/tacticool/view/scene/Scene.java
@@ -2,9 +2,11 @@ package com.anything.tacticool.view.scene;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public abstract class Scene extends ApplicationAdapter {
     protected SceneManager sm;
+    private Stage stage;
 
     protected Scene(){this.sm = SceneManager.getInstance();}
 
@@ -15,5 +17,12 @@ public abstract class Scene extends ApplicationAdapter {
 
     public void dispose(SpriteBatch batch){
         batch.dispose();
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+    public void prepareStage(){
+        this.stage = new Stage();
     }
 }
--- client/core/src/com/anything/tacticool/view/scene/Settings.java
+++ client/core/src/com/anything/tacticool/view/scene/Settings.java
@@ -1,5 +1,79 @@
 package com.anything.tacticool.view.scene;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
 public class Settings extends Scene {
 
+    private Stage stage;
+    private Skin skin;
+
+
+    @Override
+    public void prepareStage() {
+        this.stage = new Stage(new ScreenViewport());
+        this.skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+        this.skin.getFont("default-font").getData().setScale(3f);
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float uiWidth = screenWidth/3f;
+        float uiHeight = screenHeight/6f;
+        float ui_xPosition = screenWidth/2 - uiWidth/2;
+
+        TextButton mute_Button = new TextButton("Mute Game", skin);
+        TextButton closeSettings_Button = new TextButton("Close Settings", skin);
+
+        mute_Button.setPosition(ui_xPosition, screenHeight * 4/5);
+        closeSettings_Button.setPosition(ui_xPosition, screenHeight * 3/5);
+
+        mute_Button.setSize(uiWidth, uiHeight);
+        closeSettings_Button.setSize(uiWidth, uiHeight);
+
+        mute_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                //TODO code to mute/unmute the game
+            }
+        });
+        closeSettings_Button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                closeSettings();
+            }
+        });
+
+        stage.addActor(mute_Button);
+        stage.addActor(closeSettings_Button);
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+
+    @Override
+    public void render(SpriteBatch batch) {
+        ScreenUtils.clear(0,0,0,1);
+
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void dispose(SpriteBatch batch) {
+        stage.dispose();
+        batch.dispose();
+    }
+
+
+    private void closeSettings() {
+        sm.Pop();
+    }
 }
