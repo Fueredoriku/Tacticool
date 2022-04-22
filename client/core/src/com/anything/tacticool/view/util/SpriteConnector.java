6f470eb6b9334b7672a065dc390ba9919eb26560
fredrlei <fredrlei@stud.ntnu.no>
2022-04-22 16:28:55 +0200

    Started implementing a Stage on the gameview scene, where every spriteconnector holds an actor for handling inputs.

--- client/core/src/com/anything/tacticool/view/util/SpriteConnector.java
+++ client/core/src/com/anything/tacticool/view/util/SpriteConnector.java
@@ -2,17 +2,28 @@ package com.anything.tacticool.view.util;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class SpriteConnector {
 
     private SpriteConnectorEnum spriteConnectorEnum;
     private int x;
     private int y;
+    private Actor actor;
 
     public SpriteConnector(SpriteConnectorEnum spriteConnectorEnum, int x, int y) {
         this.spriteConnectorEnum = spriteConnectorEnum;
         this.x = x;
         this.y = y;
+        this.actor = new Actor();
+        actor.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+
+            }
+                          });
     }
 
     public SpriteConnectorEnum getSpriteConnectorEnum() {
@@ -25,6 +36,13 @@ public class SpriteConnector {
 
     public int getY() {
         return y;
+    }
+
+    public Actor getActor() throws Exception{
+        if (actor == null){
+            throw new Exception("The sprite has no actor tied to it!");
+        }
+        return actor;
     }
 
     /**
