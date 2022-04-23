3e96a9bca611fb91f69c089baaa72396763413af
Hundini <martin.burgos99@gmail.com>
2022-04-23 13:31:20 +0200

    Added postMoves request, now to testing if it works

--- client/core/src/httpRequests/Request.java
+++ client/core/src/httpRequests/Request.java
@@ -3,17 +3,18 @@ package httpRequests;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class Request {
-    URL url = new URL("http://localhost:8080");
-
     public Request() throws MalformedURLException {
     }
 
     public void getter() throws IOException {
+        URL url = new URL("http://localhost:8080");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         BufferedReader in = new BufferedReader(
@@ -27,6 +28,25 @@ public class Request {
         System.out.println(content);
         con.disconnect();
     }
+
+    public void postMoves(String body, int gid, int pid) throws IOException {
+        URL url = new URL(String.format("http://localhost:8080/api/move/%d/%d",gid,pid));
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        byte[] out = body.getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        con.setRequestMethod("POST");
+        con.setFixedLengthStreamingMode(length);
+        con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        con.connect();
+
+        try(OutputStream os = con.getOutputStream()) {
+            os.write(out);
+        }
+        System.out.println(body);
+        con.disconnect();
+    }
+
 
     public static void main(String[] args) throws IOException {
         Request request = new Request();
