840f579c4fc43fbd7b46116e9db7818f4c8944a8
thoraf <thoraf@stud.ntnu.no>
2022-04-24 10:49:35 +0200

    Changed opening scene to LoginView. Added actors to LoginView

--- client/core/src/com/anything/tacticool/view/scene/LoginView.java
+++ client/core/src/com/anything/tacticool/view/scene/LoginView.java
@@ -3,6 +3,7 @@ package com.anything.tacticool.view.scene;
 import com.anything.tacticool.view.util.ActorFactory;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -10,6 +11,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class LoginView extends Scene{
@@ -49,31 +51,63 @@ public class LoginView extends Scene{
         float ui_xPosition = screenWidth/2 - uiWidth/2;
         float ui_yScale = screenHeight/5f;
 
-        TextField username = (TextField) actorFactory.actor(
+        Label username_Label = (Label) actorFactory.actor(
+          new Label("Username", skin),
+                uiWidth, uiHeight, ui_xPosition, ui_yScale * 3 + uiHeight
+        );
+
+        TextField username_TextField = (TextField) actorFactory.actor(
                 new TextField("", skin),
                 uiWidth, uiHeight, ui_xPosition, ui_yScale * 3
         );
 
-        TextButton login = actorFactory.textButton(
+        TextButton login_Button = actorFactory.textButton(
                 new TextButton("Login", skin),
                 uiWidth, uiHeight, ui_xPosition, ui_yScale * 2,
                 new ClickListener() {
                     @Override
                     public void clicked(InputEvent event, float x, float y) {
-                        //joinGame();
+                        login();
                     }
                 }
         );
+
+        actorFactory.stageActors(stage, new Actor[] {username_TextField, username_Label, login_Button});
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    private void login() {
+        //TODO connect to server
+        sm.Push(new MainView());
     }
 
     @Override
     public void render(SpriteBatch batch) {
+        ScreenUtils.clear(0.5f, 0.2f, 0.5f, 1);
+        batch.begin();
+        stage.draw();
+        checkState();
+        this.username = ((TextField) stage.getActors().get(0)).getText();
+        batch.end();
+    }
 
+    private void checkState() {
+        if (stage.getActors().size != 3) {
+            throw new IllegalStateException("Expected 3 actors, instead of " + stage.getActors().size);
+        }
+        if (!(stage.getActors().get(0) instanceof TextField)) {
+            throw new IllegalStateException("Expected TextField at index 0 in Actors, instead of " + stage.getActors().get(0).getName());
+        }
     }
 
     @Override
     public void disposeEarly() {
 
+    }
+
+    @Override
+    public void dispose(SpriteBatch batch) {
+        batch.dispose();
     }
 
 }
