a25705e455af1a40c3a2d4d8699d7b0ca3e9d449
Théo <t.houle1311@gmail.com>
2022-04-24 15:03:18 +0200

    terraingrid test

--- server/src/test/java/please/tacticool/models/TerrainGridTest.java
+++ server/src/test/java/please/tacticool/models/TerrainGridTest.java
@@ -9,17 +9,24 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import please.tacticool.models.Actors.Actor;
+import please.tacticool.models.Actors.Player;
+
 @SpringBootTest
 public class TerrainGridTest {
 
+    private static TerrainGrid grid;
+    private static Player dummyPlayer;
+
     @BeforeAll
     public static void beforeAll(){
-        
+        grid = new TerrainGrid(5, 5);
+        dummyPlayer = new Player(1, new Coordinate(1,1), 10);
+        grid.setActor(dummyPlayer.getPosition(), dummyPlayer);
     }
 
     @Test
     void isValidCoordinateTest(){
-        TerrainGrid grid = new TerrainGrid(5, 5);
         for(int x = -1; x < 6; x++){
             for(int y = -1; y < 6; y ++){
                 if(x < 0 || x > 4 || y < 0 || y > 4){
@@ -31,5 +38,37 @@ public class TerrainGridTest {
             }
         }
     }
+
+    @Test
+    void moveActorTest(){
+        Coordinate origin = dummyPlayer.getPosition();
+        Coordinate target = new Coordinate(1,2);
+        assertTrue(grid.moveActor(origin, target));
+        assertTrue(grid.getTile(origin).isEmpty());
+        assertFalse(grid.getTile(target).isEmpty());
+        assertTrue(grid.getTile(target).getActor().equals(dummyPlayer));
+        dummyPlayer.setPosition(target);
+
+        origin = new Coordinate(target);
+        target = new Coordinate(1, 3);
+        assertTrue(grid.moveActor(dummyPlayer, target));
+        assertTrue(grid.getTile(origin).isEmpty());
+        assertFalse(grid.getTile(target).isEmpty());
+        assertTrue(grid.getTile(target).getActor().equals(dummyPlayer));
+
+        origin = new Coordinate(target);
+        target = new Coordinate(1, 2);
+        assertTrue(grid.moveActor(dummyPlayer.getPlayerID(), target));
+        assertTrue(grid.getTile(origin).isEmpty());
+        assertFalse(grid.getTile(target).isEmpty());
+        assertTrue(grid.getTile(target).getActor().equals(dummyPlayer));
+        dummyPlayer.setPosition(target);
+
+        Coordinate illegalMove = new Coordinate(5, 1);
+        assertThrows(IndexOutOfBoundsException.class, () -> grid.moveActor(dummyPlayer, illegalMove));
+        assertTrue(grid.getTile(target).getActor().equals(dummyPlayer));
+    }
+
+
     
 }
