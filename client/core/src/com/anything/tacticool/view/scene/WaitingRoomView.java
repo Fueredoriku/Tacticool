c9ace27fc36ab6e14f1c3deabba900dd67341858
Théo <t.houle1311@gmail.com>
2022-04-25 14:26:51 +0200

    Centered game pin text

--- client/core/src/com/anything/tacticool/view/scene/WaitingRoomView.java
+++ client/core/src/com/anything/tacticool/view/scene/WaitingRoomView.java
@@ -70,10 +70,19 @@ public class WaitingRoomView extends Scene {
         float ui_xPosition = screenWidth/2 - uiWidth/2;
         float ui_yScale = screenHeight/5f;
 
+        Label game_pin_label = (Label) actorFactory.label(
+                "Game Pin : " + this.gameID, skin,
+                uiWidth, uiHeight,
+                Gdx.graphics.getWidth()/2, ui_yScale * 3f,
+                true
+        );
+        /*
         Label game_pin_label = (Label) actorFactory.actor(
                 new Label("Game pin : " + this.gameID, skin),
                 uiWidth, uiHeight, ui_xPosition - uiWidth/1.5f, ui_yScale * 3f
         );
+
+         */
 
         TextButton start_game_button = actorFactory.textButton(
                 new TextButton("Start Game", skin),
